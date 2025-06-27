19dd97e516b20068798401087797849153d6ee5b
GAUTHAMI SANKARAMANGALAM <67738017+GAUTHAMI-S@users.noreply.github.com>
2025-06-27 23:34:01 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -47,5 +47,10 @@ app.use('/api/messages', messageRouter)
 // connect to mongodb
 await connectDB()
 
+if (process.env.NODE_ENV!=='production'){
 const PORT = process.env.PORT || 5005;
 server.listen(PORT, () => console.log('Server is running on PORT:', + PORT))
+}
+
+// export server for vercel
+export default server;
